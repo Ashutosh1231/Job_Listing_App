93ac89a552a0195ad875654241bd5527c216c31c
Ashutosh Kasat <akasat54@gmail.com>
2023-10-22 00:00:13 +0530

    Initial commit
    Initial commit

---
+++ Backend/jobListingApp/src/test/java/com/Ashutosh/jobListingApp/JobListingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ashutosh.jobListingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
